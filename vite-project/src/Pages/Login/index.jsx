8824ef7f71c708d8ff836d0ca83a544d8cc7bad2
JBevc <141636441+JBevc@users.noreply.github.com>
2024-10-05 11:49:39 -0300

    cambios login

---
+++ vite-project/src/Pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import { Image } from "antd";
+import "./Login.css";
+
+export function Login() {
+  return (
+    <>
+      <Image width={400} src="vite-project/src/assets/Logo.png" />
+      <h1> Fakestagram</h1>
+    </>
+  );
+}
