237b9e21710875841bdbe7976665ed3bf5a074fd
LuciaOliveraa <lucia.olivera@correo.ucu.edu.uy>
2024-10-11 11:26:37 -0300

    Avances en Home

--- vite-project/src/Pages/Home/index.jsx
+++ vite-project/src/Pages/Home/index.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Layout } from "antd";
 import { SiderContent } from "../../Components/SiderContent";
+import { ViewProfileSuggestions } from "../../Components/ViewProfileSuggestions";
+//import "./Home.module.css";
+import { ProfilePreView } from "../../Components/ProfilePreView";
 
 const { Header, Footer, Sider, Content } = Layout;
 
 export function Home() {
     return (
-        <Layout>
-            <Sider width="25%" >
+        <Layout style={{minHeight: '1400px', minWidth: '1500px'}}>
+            <Sider theme={"light"}>
                 <SiderContent></SiderContent>
             </Sider>
+            <Layout>
+                <Header theme={"light"}>
+                    <ViewProfileSuggestions/>
+                </Header>
+                <Content>
+                    Home
+                    <ProfilePreView picture={"https://bulma.io/assets/images/placeholders/32x32.png"} userName={"pepeymarta"} />
+                </Content>
+                <Footer>
+                    footer
+                </Footer>
+            </Layout>
         </Layout>
     );
 }
