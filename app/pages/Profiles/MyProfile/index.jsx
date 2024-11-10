520878f4abdaca94b56fa12078be7f2241178aa3
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2024-11-10 11:10:46 -0300

    bottomBar solucionada

--- app/pages/Profiles/MyProfile/index.jsx
+++ app/pages/Profiles/MyProfile/index.jsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, StyleSheet, Text, View, TextInput, Pressable, Image, ImageBackground} from 'react-native';
+import BottomBarTabs from '../../../components/Bars/BottomBarTabs';
 
 export default function Profile() {
 
     return (
-        <SafeAreaView>
-            <Text>My profileeeeeeee</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>My profileee</Text>
+            <BottomBarTabs atPage='Profile' />
         </SafeAreaView>
     )
 }
